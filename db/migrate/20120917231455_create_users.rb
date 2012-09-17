d28f2d4d5766f14acfa9c87286aab703561bce7d
Mike Butsko <butsko.7@osu.edu>
2012-09-17 19:50:02 -0400

    Files in working order. Next stop, form

--- db/migrate/20120917231455_create_users.rb
+++ db/migrate/20120917231455_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :name_n
-      t.integer, :emplid
+      t.string :name_n
+      t.integer :emplid
       t.string :avatar
 
       t.timestamps
