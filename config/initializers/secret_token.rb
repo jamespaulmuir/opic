c4086c08e2cf57a21b3207500cb7374dc5ef62cd
Mike Butsko <butsko.7@osu.edu>
2012-09-17 16:17:48 -0400

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opic::Application.config.secret_token = 'f261f9819663e796cc13cec1a91d1fddccb8684975d2eee67168f200dc575d38a3d0e852184fbd5d0eedd08153beb78fec211580bac84bd2dd27f58b2b2f4d15'
