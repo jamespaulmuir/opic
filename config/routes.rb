2315cd5aadf32e13d69fcd0773f668046d6a5578
Mike Butsko <butsko.7@osu.edu>
2012-09-18 16:08:28 -0400

    Friendly filename

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Opic::Application.routes.draw do
 
   resources :avatars
 
-  get "/users/:name_n" => "users#show", :constraints => {:name_n => /\D+\.\d+/ }
   get '/:name_n'       => 'users#show', :constraints => {:name_n => /\D+\.\d+/ }
 
   root :to => 'avatars#new'
