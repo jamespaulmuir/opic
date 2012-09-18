0163adf248db9803373103816efe70bdc9a77182
John Colvin <colvin.john@gmail.com>
2012-09-18 15:29:15 -0400

    Downcased name.# in user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find_by_name_n(params[:name_n])
+    name_n = params[:name_n].downcase
+    @user = User.find_by_name_n(name_n)
 
     if @user
       @avatar = @user.avatars.last
       send_file @avatar.picture.thumb.path
     else
-      @user = User.new(:name_n => params[:name_n])
+      @user = User.new(:name_n => name_n)
       redirect_to @user.gravatar_url(:default => params[:default] || default_url)
     end
 
