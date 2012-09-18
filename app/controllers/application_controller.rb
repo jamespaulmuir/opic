41dcffd3b3fc3f8ba7a1f3e9d7c012b175cebb4a
Mike Butsko <butsko.7@osu.edu>
2012-09-18 15:31:27 -0400

    widths and aspects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from AvatarUploader::InvalidImageRequest do |exception|
+    render :text => "Invalid Image Request: #{exception.message}", :status => 404
+  end
 end
