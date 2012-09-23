8b29bd57560b577f52ec892d2fbb99267af62364
Mike Butsko <butsko.7@osu.edu>
2012-09-22 23:13:13 -0400

    precompile assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -55,6 +55,19 @@ namespace :deploy do
   end
 end
 
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true, :cowboy_deploy => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end 
+end
+
 after "deploy:create_symlink" do
   run [
     "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml",
