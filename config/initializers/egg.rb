5669cd96557def62e59fede36d1a7e9cc4c7f438
Mike Butsko <butsko.7@osu.edu>
2012-09-26 11:03:57 -0400

    Enable X-Send

--- config/initializers/egg.rb
+++ config/initializers/egg.rb
@@ -1,3 +1,4 @@
-#Egg.api_key = 'd3f21d768e21aced1308e2533830fced'
-Egg.api_key = '04f483305c8c362fb747bf9d390b8ce3'
+Egg.api_key = 'd3f21d768e21aced1308e2533830fced'
+#Test Key
+#Egg.api_key = '04f483305c8c362fb747bf9d390b8ce3'
 Egg.url     = 'https://egg.asc.ohio-state.edu/events'
