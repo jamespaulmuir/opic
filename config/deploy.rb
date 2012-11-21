622e4e12ae3c29561075705ebc9b20bf44187fa7
Mike Butsko <butsko.7@osu.edu>
2012-11-21 16:20:30 -0500

    assets?

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,7 +58,7 @@ end
 
 namespace :deploy do
   namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true, :cowboy_deploy => true } do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
       from = source.next_revision(current_revision)
       if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets | wc -l").to_i > 0
         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
@@ -79,3 +79,4 @@ before "deploy:assets:precompile" do
 end
 
 after "deploy", "delayed_job:restart", "deploy:cleanup"
+after "deploy:create_symlink", "deploy:assets:precompile"
