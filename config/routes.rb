d2074d8ead5ec87679cdcde4717c112b15d03831
mikegee <gee.24@osu.edu>
2012-12-07 19:51:22 -0500

    no crazy in prod, and common routes should match earliest

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,23 @@
 Opic::Application.routes.draw do
 
+  get '/:name_n' => 'pictures#show', :constraints => {:name_n => /\D+\.\d+/ }
+  get '/:hash'   => 'pictures#show'
+
+  root :to => 'avatars#new'
+
   resources :avatars
-  
+
   post '/use_gravatar'  => 'avatars#use_gravatar'
 
   put '/opt_out'      =>  'users#opt_out'
   put '/opt_in'       =>  'users#opt_in'
 
-  root :to => 'avatars#new'
-
   %w{ developers faq about }.each do |page|
     get "/#{page}" => "pages##{page}"
   end
- 
+
   post '/api/avatars' => 'api#create'
 
-  get '/crazy'        => 'pictures#index'
-
-  get '/:name_n'       => 'pictures#show', :constraints => {:name_n => /\D+\.\d+/ }
-  get '/:hash'         => 'pictures#show'
-
+  get '/crazy' => 'pictures#index' unless Rails.env.production?
 
 end
